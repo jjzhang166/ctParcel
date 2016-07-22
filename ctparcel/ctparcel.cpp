d240f5982197c304272d2e95b8e96a94ca181c46
codetask <codetask@outlook.com>
2016-07-23 02:06:31 +0800

    test2

--- ctparcel/ctparcel.cpp
+++ ctparcel/ctparcel.cpp
@@ -38,8 +38,14 @@ namespace ctWin32Dialog
 	{
 		string partname;
 		COLORREF color;
-		POINT lstart;
-		POINT lend;
+		POINT lstart, lend;
+	};
+	struct BmpInfo
+	{
+		string partname;
+		string filename;
+		int cx, cy;
+		int width, height;
 	};
 
 	//////////////////////////////////////////////////////////////////////////
@@ -117,6 +123,7 @@ namespace ctWin32Dialog
 						GetClientRect( hWnd, &rect );
 						hdc = BeginPaint( hWnd, &ps );
 						
+						// brush color
 						auto brushWindow = [&](COLORREF color,RECT rt) {
 							HBRUSH brush = CreateSolidBrush( color );		//内容
 							HPEN pen = CreatePen( PS_NULL, 0, 0 );			//线条
@@ -126,7 +133,6 @@ namespace ctWin32Dialog
 							DeleteObject( pen );
 							DeleteObject( brush );
 						};
-						
 						// brush background-color
 						brushWindow( bgColor, rect );
 						// brush forecolor
@@ -140,9 +146,23 @@ namespace ctWin32Dialog
 							LineTo( hdc, ex, ey );
 							DeleteObject( pen );
 						};
-						for(auto it : lineInfos)
+						for(auto &it : lineInfos)
 							drawline( it.color, it.lstart.x, it.lstart.y, it.lend.x, it.lend.y );
 						
+						// show bmp
+						auto drawbmp = [&]( string& filename, int cx, int cy, int width, int height ) {
+							//这里如果要求效率高的话 要提前load
+							HBITMAP hbmp = (HBITMAP)LoadImageA( NULL, filename.c_str(),
+								IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE | LR_CREATEDIBSECTION );
+							HDC hMemDC = CreateCompatibleDC( hdc );
+							SelectObject( hMemDC, hbmp );
+							BitBlt( hdc, cx, cy, width, height, hMemDC, 0, 0, SRCCOPY );
+							DeleteObject( hbmp );
+							DeleteObject( hMemDC );
+						};
+						for(auto &it : bmpInfos)
+							drawbmp( it.filename, it.cx, it.cy, it.width, it.height );
+
 						EndPaint( hWnd, &ps );
 						return FALSE;
 					}
@@ -152,7 +172,7 @@ namespace ctWin32Dialog
 					break;
 
 				default:
-					//回去调用defwndproc
+					// 回去调用defwndproc
 					return -1;
 			}
 			return 0;
@@ -185,13 +205,13 @@ namespace ctWin32Dialog
 			RegisterClassExW( &wcex );
 
 			// create main window
-			HWND hWnd = CreateWindow( szClass, TEXT( "ctDialog" ), WS_OVERLAPPEDWINDOW,
+			HWND hWnd = CreateWindow( szClass, TEXT( "ctDialog" ), WS_OVERLAPPEDWINDOW&~WS_MAXIMIZEBOX,
 				CW_USEDEFAULT, 0, xWidth, xHeight, nullptr, nullptr, appInstance, nullptr );
 
 			if(hWnd)
 			{
 				hMainDlg = hWnd;
-				//添加到ctDialogInfos记录中,以便中转callback调用类的函数
+				// 添加到ctDialogInfos记录中,以便中转callback调用类的函数
 				ctDialogInfos.push_back( {hWnd,this} );
 
 				ShowWindow( hWnd, 5 );
@@ -234,7 +254,7 @@ namespace ctWin32Dialog
 			DWORD windowId = getwindowid();
 
 			// 创建窗口
-			HWND hWnd = CreateWindowA( className.c_str(), windowName.c_str(), WS_VISIBLE | WS_CHILD | partType,
+			HWND hWnd = CreateWindowA( className.c_str(), windowName.c_str(),  WS_VISIBLE | WS_CHILD | partType,
 				x, y, width, height, hMainDlg, (HMENU)windowId, appInstance, nullptr );
 			//字体
 			HFONT font = (HFONT)GetStockObject( DEFAULT_GUI_FONT );
@@ -250,17 +270,18 @@ namespace ctWin32Dialog
 			regCommandMap.insert( pair<int, CommandCallback>( windowId, proc ) );
 			return true;
 		}
+		// partName == "*" 时为删除所有
 		bool deletePart( string partName )
 		{
 			bool ret = false;
 			for(auto &it = allcreated.begin(); it != allcreated.end();)
 			{
-				if(it->partname == partName)
+				if(it->partname == partName || partName=="*" )
 				{
-					//删除partWindow
+					// 删除partWindow
 					DestroyWindow( it->hWnd );
-					//取消注册的函数
-					for(auto itmap = regCommandMap.begin(); itmap != regCommandMap.end(); itmap++)
+					// 取消注册的函数
+					for(auto &itmap = regCommandMap.begin(); itmap != regCommandMap.end(); itmap++)
 					{
 						if(itmap->first == it->windowId)
 						{
@@ -268,7 +289,7 @@ namespace ctWin32Dialog
 							break;
 						}
 					}
-					//删除其他记录
+					// 删除其他记录
 					allcreated.erase( it++ );
 					ret = true;
 					continue;
@@ -277,7 +298,30 @@ namespace ctWin32Dialog
 			}
 			return ret;
 		}
+		void deleteAllPart()
+		{
+			deletePart( "*" );
+		}
+		// 直接清空当前面板
+		void clearDlg()
+		{
+			deleteAllPart();
+			eraseDrewAll();
+		}
 
+		//
+		// 获取创建过的控件句柄
+		HWND getWnd(string partName)
+		{
+			for(auto &it : allcreated)
+			{
+				if(it.partname == partName)
+					return it.hWnd;
+			}
+			return 0;
+		}
+
+		//
 		// 创建控件
 		// 调用时带上partName时才可以删除指定的控件  
 		// (当然也可以使用默认的partname == "static","button"...)
@@ -293,6 +337,51 @@ namespace ctWin32Dialog
 			CommandCallback proc = nullptr, string partName = "button" )
 		{
 			return createPart( "button", content, BS_PUSHBUTTON, x, y, width, height, partName,proc );
+		}
+
+		//
+		// 画控件 (注意:这样的控件不会保存进allcreated)
+		//
+		// 删除已经画上去的控件 (仅限drawxxxx上去的)
+		// 
+		template <typename T>
+		void eraseDrewPart( T& infos, string partName )
+		{
+			for(auto &it = infos.begin(); it != infos.end(); it++)
+			{
+				if(it->partname == partName)
+				{
+					infos.erase( it );
+					InvalidateRect( hMainDlg, NULL, FALSE );
+					break;
+				}
+			}
+		}
+		void eraseDrewAll()
+		{
+			lineInfos.clear();
+			bmpInfos.clear();
+			InvalidateRect( hMainDlg, NULL, FALSE );
+		}
+		// draw line (保存记录后从wm_paint里画的)
+		// line =>  sx,sy -> ex,ey
+		inline void drawLine( int sx, int sy, int ex, int ey, COLORREF col = RGB( 0, 0, 0 ), string partName = "line" )
+		{
+			lineInfos.push_back( {partName, col,{sx,sy},{ex,ey}} );
+		}
+		inline void eraseLine( string partName )
+		{
+			eraseDrewPart<list<LineInfo>>( lineInfos ,partName);
+		}
+
+		// draw images(bmp)
+		void drawBmp( string filename,int cx, int cy, int width, int height, string partName = "bmp" )
+		{
+			bmpInfos.push_back( {partName, filename,cx,cy,width,height} );
+		}
+		inline void eraseBmp( string partName )
+		{
+			eraseDrewPart<list<BmpInfo>>( bmpInfos, partName );
 		}
 
 		// 主对话框样式
@@ -316,29 +405,9 @@ namespace ctWin32Dialog
 			forecolorRect = foreRt;
 			InvalidateRect( hMainDlg, NULL, FALSE );
 		}
-		
-		// draw line (保存记录后从wm_paint里画的)
-		// line =>  sx,sy -> ex,ey
-		void drawLine(int sx,int sy,int ex,int ey, COLORREF col=RGB(0,0,0),string partName="line")
-		{
-			lineInfos.push_back( {partName, col, {sx,sy}, {ex,ey}} );
-		}
-		void eraseLine(string partName)
-		{
-			for(auto &it = lineInfos.begin(); it != lineInfos.end(); it++)
-			{
-				if(it->partname == partName)
-				{
-					lineInfos.erase( it );
-					InvalidateRect( hMainDlg, NULL, FALSE );
-					break;
-				}
-			}
-		}
-		
+	
 		//
 		// static回调 (中转)
-		//
 		static LRESULT CALLBACK staticWndCallback( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
 		{
 			for(auto &it : ctDialogInfos)
@@ -362,6 +431,8 @@ namespace ctWin32Dialog
 		HINSTANCE appInstance;
 		COLORREF bgColor, fontColor,foreColor;
 		RECT forecolorRect;
+		// 记录所有bmpImages的信息
+		list<BmpInfo> bmpInfos;
 		// 记录所有LineTo的信息
 		list<LineInfo> lineInfos;
 		// 记录当前对话框中的所有组件信息
@@ -382,12 +453,32 @@ namespace ctWin32Dialog
 //////////////////////////////////////////////////////////////////////////
 ctWin32Dialog::ctDialog ctd;
 
-int CALLBACK xproc( HWND hWnd, DWORD windowId )
+int CALLBACK page1( HWND hWnd, DWORD windowId );
+
+int CALLBACK page2( HWND hWnd, DWORD windowId )
 {
-	ctd.deletePart( "static" );
+	ctd.clearDlg();
+
 	ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,60} );
 	ctd.drawLine( 0, 60, 510, 60, (COLORREF)0xA0A0A0 );
-	ctd.createbutton( "< 上一步(B)", 200, 300, 85, 22 );
+	ctd.createbutton( "< 上一步(B)", 200, 300, 85, 22, page1 );
+	ctd.createbutton( "下一步(N) >", 300, 300, 85, 22 );
+	ctd.createbutton( "取消", 400, 300, 85, 22 );
+	return 0;
+}
+
+int CALLBACK page1( HWND hWnd, DWORD windowId )
+{
+	ctd.clearDlg();
+
+	ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
+	ctd.drawBmp( "e:\\WizardImage.bmp", 0, 0, 160, 290 );
+	ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,290} );
+
+	ctd.createText( "这个是标题", 200, 30, 200, 22, true );
+	ctd.createText( "内容很长很长很长很长", 200, 60, 200, 22 );
+	ctd.createbutton( "下一步(N) >", 300, 300, 85, 22, page2 );
+	ctd.createbutton( "取消", 400, 300, 85, 22 );
 	return 0;
 }
 
@@ -398,13 +489,7 @@ int WINAPI WinMain( _In_ HINSTANCE hInstance,
 {
 	ctd.createMainDialog( 510, 370 );
 	ctd.setTitle( "呼叫地面测试中心" );
-	ctd.drawLine( 0, 285, 510, 285,(COLORREF)0xA0A0A0 );
-	ctd.setForecolor( RGB(255,255,255) , {0,0,ctd.hMainDlgRect.right,285} );
-
-	ctd.createText( "这个是标题", 100, 30, 200, 22, true );
-	ctd.createText( "内容很长很长很长很长", 100, 60, 200, 22 );
-	ctd.createbutton( "下一步(N) >", 300, 300, 85, 22, xproc );
-	ctd.createbutton( "取消", 400, 300, 85, 22 );
+	page1(0,0);
 	ctd.showMainDialog();
 	return 0;
 }
