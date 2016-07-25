3c53bd2f00ae704e6bb8d047a9981f09c9c69808
codetask <codetask@outlook.com>
2016-07-26 00:19:18 +0800

    add <read setup-string from file>

--- ctparcel/ctWizard.h
+++ ctparcel/ctWizard.h
@@ -4,87 +4,6 @@
 
 namespace ctWin32Wizard
 {
-	// PARTCALLBACK(prc) 生成一个lambda 用来调用成员函数的回调函数
-	#define PARTCALLBACK(proc) [=](HWND hDlg,DWORD windowId)->int{return proc(hDlg,windowId);}
-
-	// 在生成的最终exe的文件后面添加一个结构,这个结构包含了必须的UI图片/配置/压缩文件等
-	struct fileblock
-	{
-		char relativepath[80];			// 临时文件="tmp" or 相对路径 : aa
-		char filename[20];				// 文件名   : a.dll   >> 那么这个文件的路径: setuppath/aa/a.dll
-		DWORD filesize;					// 大小
-		byte* filebuf;					// 内容
-	};
-
-	struct addedSector
-	{
-		DWORD verifycode;			//
-		char compressType[10];		// 压缩方式
-		char programName[20];		// 安装包的程序名
-
-		int nfiles;
-		fileblock fileblocks[1];	// 文件列表
-	};
-
-
-	/*
-	[文件]
-	每个文件拥有一个结构
-	struct fileblock
-	{
-		char relativepath[80];			// 相对路径 : aa
-		char filename[20];				// 文件名   : a.dll   >> 那么这个文件的路径: setuppath/aa/a.dll
-		DWORD filesize;					// 大小
-		byte * filebuf;					// 内容
-	};
-	将所有fileblock顺序放入一块大内存中
-	fileblock[xx]
-
-	-----------(规定)-----------
-
-	默认使用第1个文件(txt-file)作为安装时的字符串读取:
-	**************************************
-	[follow this:]
-
-	安装 - %s
-	< 上一步(B)
-	下一步(N) >
-	取消
-	继续
-	完成(F)
-	欢迎使用 %s 安装向导
-	现在将安装 %s。
-	建议你在继续之前关闭其他应用程序。
-	点击"下一步"继续,或者点击"取消"退出安装。
-	选择目标位置
-	%s 要安装到哪里？
-	安装程序将把 %s 安装到以下文件夹。
-	若要继续,请点击"下一步"。 如果你要换一个文件夹,请点击"浏览"。
-	浏览(R)。。。
-	准备安装
-	安装程序准备在你的电脑上安装。
-	点击"安装"继续,如果你想修改设置请点击"上一步"。
-	正在安装
-	正在安装 %s, 请稍后。。。
-	正在提取文件。。。
-	%s 安装向导完成
-	安装程序已在您的电脑中安装了 %s 。 此应用程序可以通过选择安装的快捷方式运行。
-	单击'完成'退出安装程序。
-	运行 %s
-	退出安装
-	安装未完成!如果你现在退出,程序将无法完成！
-	要退出安装吗？
-
-	**************************************
-
-	默认使用第2,3,4个文件作为3个安装时的图片:
-	WizardImage.bmp
-	SmallWizardImage.bmp
-	folders.bmp
-	*/
-
-
-
 	//
 	// simple load pe class
 	//	
@@ -144,6 +63,74 @@ namespace ctWin32Wizard
 	};
 
 
+	// PARTCALLBACK(prc) 生成一个lambda 用来调用成员函数的回调函数
+	#define PARTCALLBACK(proc) [=](HWND hDlg,DWORD windowId)->int{return proc(hDlg,windowId);}
+	// 在生成的最终exe的文件后面添加一个结构,这个结构包含了必须的UI图片/配置/压缩文件等
+	#define RELATIVEPATH_TMP	".tmp"
+	struct fileblock
+	{
+		char relativepath[150];			// 临时文件=".tmp" or 相对路径 : aa/bb/
+		char filename[50];				// 文件名   : a.dll   >> 那么这个文件的路径: setuppath/aa/bb/a.dll
+		DWORD filesize;					// 大小
+										//
+										// +sizeof(fileblock) 之后就是文件内容 byte* filebuf
+	};
+	struct addedSector
+	{
+		DWORD verifycode;				// 0
+
+		char compressType[10];			// 压缩方式
+		char programName[20];			// 安装包的程序名
+
+		int nfiles;						// 总文件数量
+										//
+										// +sizeof(addedSector) 之后就是每个文件块 fileblock
+	};
+
+	/*
+	-----------(规定)-----------
+
+	默认使用第1个文件(txt-file)作为安装时的字符串读取:
+	**************************************
+	[follow this:]  gb2312
+
+	安装 - %s
+	< 上一步(B)
+	下一步(N) >
+	取消
+	继续
+	安装(I)
+	完成(F)
+	欢迎使用 %s 安装向导
+	现在将安装 %s。
+	建议你在继续之前关闭其他应用程序。
+	点击"下一步"继续,或者点击"取消"退出安装。
+	选择目标位置
+	%s 要安装到哪里？
+	安装程序将把 %s 安装到以下文件夹。
+	若要继续,请点击"下一步"。 如果你要换一个文件夹,请点击"浏览"。
+	浏览(R)...
+	准备安装
+	安装程序准备在你的电脑上安装。
+	点击"安装"继续,如果你想修改设置请点击"上一步"。
+	正在安装
+	正在安装 %s, 请稍后。。。
+	正在提取文件。。。
+	%s 安装向导完成
+	安装程序已在您的电脑中安装了 %s 。 此应用程序可以通过选择安装的快捷方式运行。
+	单击'完成'退出安装程序。
+	运行 %s
+	退出安装
+	安装未完成!如果你现在退出,程序将无法完成！
+	要退出安装吗？
+
+	**************************************
+
+	默认使用第2,3,4个文件作为3个安装时的图片:
+	2: WizardImage.bmp
+	3: SmallWizardImage.bmp
+	4: folders.bmp
+	*/
 
 	//
 	// Wizard类
@@ -162,11 +149,100 @@ namespace ctWin32Wizard
 			GetModuleFileNameA( NULL, tmp, 260 );
 			if(selfpe.loadfile( tmp ))
 			{
-				//从最后一个区段得到之前文件的大小,超出的就是添加的区段了
+				//从最后一个区段得到之前文件的大小,超出的就是添加的数据了
 				PIMAGE_SECTION_HEADER lastsec = selfpe.secheader + selfpe.seccount -1;
-				return (addedSector*)( lastsec->PointerToRawData + lastsec->SizeOfRawData );
+				return (addedSector*)(selfpe.filebuf + lastsec->PointerToRawData + lastsec->SizeOfRawData );
 			}
 			return nullptr;
+		}
+
+		//获取指定的文件块位置
+		fileblock* getFileBlock(int ifile)
+		{
+			if(ifile < addedsec->nfiles)
+			{
+				//先到第一个
+				fileblock* fb = (fileblock*)((DWORD)addedsec + sizeof( addedSector ));
+				//一直遍历到指定的位置
+				for(int i = 0; i < ifile; i++)
+					fb = (fileblock*)((DWORD)fb + fb->filesize + sizeof( fileblock ));
+				return fb;
+			}
+			return nullptr;
+		}
+
+		//释放文件到指定位置
+		void outputFile(int ifile)
+		{
+			fileblock* fb = getFileBlock( ifile );
+			if(fb)
+			{
+				char extractPath[260] = {0};
+				if(strcmp( fb->relativepath, ".tmp" ) == 0)
+				{
+					//临时文件,直接释放到临时目录
+					GetTempPathA( 260, extractPath );
+					lstrcatA( extractPath, fb->filename );
+				}
+
+				byte* filebuf = (byte*)((DWORD)fb + sizeof( fileblock ));
+				FILE *f = nullptr;
+				fopen_s( &f, extractPath, "wb" );
+				if(f)
+				{
+					fwrite( filebuf, 1, fb->filesize, f );
+					fclose( f );
+				}
+
+				//MessageBoxA( 0, extractPath, 0, 0 );
+			}
+		}
+
+		//在temp path里的文件路径
+		std::string getTempFile( char* fileName )
+		{
+			char tmp[260];
+			GetTempPathA( 260, tmp );
+			lstrcatA( tmp, fileName );
+			return std::string( tmp );
+		}
+		#define  TMP_WizardImage		getTempFile("WizardImage.bmp").c_str()
+		#define  TMP_WizardSmallImage	getTempFile("WizardSmallImage.bmp").c_str()
+		#define  TMP_folders			getTempFile("folders.bmp").c_str()
+		#define  TMP_str				getTempFile("str.txt").c_str()
+		
+		//到临时文件夹里去获取str.txt指定的文字
+		//和programName进行格式化
+		std::string getString( int snum )
+		{
+			char tmp[200] = {0}, tmpall[300] = {0};
+			FILE* f = nullptr;
+			fopen_s( &f, TMP_str, "rt" );
+			if(f)
+			{
+				int i = 0;
+				while(fgets( tmp, 200, f ))
+				{
+					if(++i == snum)
+						break;
+				}
+				if(tmp[0])
+					wsprintfA( tmpall,(PCSTR)tmp,addedsec->programName );
+				fclose( f );
+			}
+			return std::string( tmpall );
+		}
+		#define GETSETUPSTRING(i) getString(i).c_str()
+
+
+		//初始化需要安装的一些信息
+		void initSetupInfomation()
+		{
+			addedsec = getAddedSector();
+
+			//释放临时文件到临时文件夹
+			for(int i=0;i<4;i++)
+				outputFile(i);
 		}
 
 		//////////////////////////////////////////////////////////////////////////
@@ -174,18 +250,14 @@ namespace ctWin32Wizard
 	public:
 		ctWizard() : step(0)
 		{
-			addedsec = getAddedSector();
-			char tmp[200];
-			wsprintfA( tmp, "%X", addedsec );
-			MessageBoxA( 0, tmp, 0, 0 );
-
+			initSetupInfomation();
 			start();
 		}
 		
 		void start(int w=510,int h=370)
 		{
 			ctd.createMainDialog( w, h );
-			ctd.setTitle( "安装 - %s" );
+			ctd.setTitle( GETSETUPSTRING(1) );
 			page1( ctd.hMainDlg, 0 );
 			
 			ctd.showMainDialog();
@@ -196,15 +268,15 @@ namespace ctWin32Wizard
 			ctd.clearDlg();
 
 			ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
-			ctd.drawBmp( "e:\\WizardImage.bmp", 0, 0, 160, 290 );
+			ctd.drawBmp( TMP_WizardImage, 0, 0, 160, 290 );
 			ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,290} );
-			ctd.createText( "欢迎使用 %s 安装向导", 180, 20, 300, 22, 16 );
-			ctd.createText( "现在将安装 %s",180,50,300,25);
-			ctd.createText( "建议你在继续之前关闭其他应用程序.", 180, 75, 300, 25 );
-			ctd.createText( "点击\"下一步\"继续,或者点击\"取消\"退出安装.", 180, 100, 300, 25 );
+			ctd.createText( GETSETUPSTRING(8), 180, 20, 300, 40, 16 );
+			ctd.createText( GETSETUPSTRING(9),180,65,300,25);
+			ctd.createText( GETSETUPSTRING(10), 180, 90, 300, 25 );
+			ctd.createText( GETSETUPSTRING(11), 180, 115, 300, 25 );
 
-			ctd.createbutton( "下一步(N) >", 300, 300, PARTCALLBACK( page2 ) );
-			ctd.createbutton( "取消", 400, 300, PARTCALLBACK( cancel ) );
+			ctd.createbutton( GETSETUPSTRING(3), 300, 300, PARTCALLBACK( page2 ) );
+			ctd.createbutton( GETSETUPSTRING(4), 400, 300, PARTCALLBACK( cancel ) );
 			return 0;
 		}
 		int CALLBACK page2( HWND hDlg, DWORD windowId )
@@ -215,17 +287,17 @@ namespace ctWin32Wizard
 			ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,60} );
 			ctd.drawLine( 0, 60, 510, 60, (COLORREF)0xA0A0A0 );
 			ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
-			ctd.drawBmp( "e:\\WizardSmallImage.bmp", 435, 0, 55, 55 );
-			ctd.createText( "选择目标位置", 20, 15, 200, 15, 12 );
-			ctd.createText( "%s 要安装到哪里?", 40, 35, 200, 15 );
-			ctd.drawBmp( "e:\\folders.bmp", 40, 75, 36, 36 );
-			ctd.createText( "安装程序将把 %s 安装到以下文件夹.", 90, 85, 200, 15 );
-			ctd.createText( "若要继续,请点击\"下一步\". 如果你要换一个文件夹,请点击\"浏览\".", 40, 120, 400, 15 );
+			ctd.drawBmp( TMP_WizardSmallImage, 435, 0, 55, 55 );
+			ctd.createText( GETSETUPSTRING(12), 20, 15, 200, 15, 12 );
+			ctd.createText( GETSETUPSTRING( 13 ), 40, 35, 300, 15 );
+			ctd.drawBmp( TMP_folders, 40, 75, 36, 36 );
+			ctd.createText( GETSETUPSTRING( 14 ), 90, 85, 350, 15 );
+			ctd.createText( GETSETUPSTRING( 15 ), 40, 120, 400, 15 );
 			ctd.createEdit( 40, 145, 300,20 , "path","c:\\testPath");
-			ctd.createbutton( "浏览(R)...", 350, 144, PARTCALLBACK( choosefile ) );
-			ctd.createbutton( "< 上一步(B)", 200, 300, PARTCALLBACK( page1 ) );
-			ctd.createbutton( "下一步(N) >", 300, 300, PARTCALLBACK( page3 ) );
-			ctd.createbutton( "取消", 400, 300, PARTCALLBACK( cancel ) );
+			ctd.createbutton( GETSETUPSTRING( 16 ), 350, 144, PARTCALLBACK( choosefile ) );
+			ctd.createbutton( GETSETUPSTRING(2), 200, 300, PARTCALLBACK( page1 ) );
+			ctd.createbutton( GETSETUPSTRING(3), 300, 300, PARTCALLBACK( page3 ) );
+			ctd.createbutton( GETSETUPSTRING(4), 400, 300, PARTCALLBACK( cancel ) );
 			return 0;
 		}
 		int CALLBACK page3( HWND hDlg, DWORD windowId )
@@ -237,16 +309,16 @@ namespace ctWin32Wizard
 			ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,60} );
 			ctd.drawLine( 0, 60, 510, 60, (COLORREF)0xA0A0A0 );
 			ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
-			ctd.drawBmp( "e:\\WizardSmallImage.bmp", 435, 0, 55, 55 );
-			ctd.createText( "准备安装", 20, 15, 200, 15, 12 );
-			ctd.createText( "安装程序准备在你的电脑上安装.", 40, 35, 200, 15 );
-			ctd.createText( "点击\"安装\"继续,如果你想修改设置请点击\"上一步\".", 40, 75, 400, 15 );
+			ctd.drawBmp( TMP_WizardSmallImage, 435, 0, 55, 55 );
+			ctd.createText( GETSETUPSTRING(17), 20, 15, 200, 15, 12 );
+			ctd.createText( GETSETUPSTRING( 18 ), 40, 35, 200, 15 );
+			ctd.createText( GETSETUPSTRING( 19 ), 40, 75, 400, 15 );
 			ctd.createEdit( 40, 100, 400, 170, "lastshow" );
 			PostMessageW( ctd.getWnd("lastshow"), EM_SETREADONLY, 1, 0 );		//设置为只读
 			ctd.setEditText( "lastshow", "目标位置:\r\n\tc:\\testSetup" );
-			ctd.createbutton( "< 上一步(B)", 200, 300, PARTCALLBACK( page2 ) );
-			ctd.createbutton( "安装(I)", 300, 300, PARTCALLBACK( page4 ) );
-			ctd.createbutton( "取消", 400, 300, PARTCALLBACK( cancel ) );
+			ctd.createbutton( GETSETUPSTRING(2), 200, 300, PARTCALLBACK( page2 ) );
+			ctd.createbutton( GETSETUPSTRING(6), 300, 300, PARTCALLBACK( page4 ) );
+			ctd.createbutton( GETSETUPSTRING(4), 400, 300, PARTCALLBACK( cancel ) );
 			return 0;
 		}
 		int CALLBACK page4( HWND hDlg, DWORD windowId )
@@ -257,11 +329,11 @@ namespace ctWin32Wizard
 			ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,60} );
 			ctd.drawLine( 0, 60, 510, 60, (COLORREF)0xA0A0A0 );
 			ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
-			ctd.drawBmp( "e:\\WizardSmallImage.bmp", 435, 0, 55, 55 );
-			ctd.createText( "正在安装", 20, 15, 200, 15, 12 );
-			ctd.createText( "正在安装 %s, 请稍后...", 40, 35, 200, 15 );
-			ctd.createText( "正在提取文件...", 40, 75, 400, 15 );
-			ctd.createbutton( "取消", 400, 300, PARTCALLBACK( cancel ) );
+			ctd.drawBmp( TMP_WizardSmallImage, 435, 0, 55, 55 );
+			ctd.createText( GETSETUPSTRING( 20 ), 20, 15, 200, 15, 12 );
+			ctd.createText( GETSETUPSTRING( 21 ), 40, 35, 300, 15 );
+			ctd.createText( GETSETUPSTRING( 22 ), 40, 75, 400, 15 );
+			ctd.createbutton( GETSETUPSTRING(3), 400, 300, PARTCALLBACK( cancel ) );
 			
 			//这里可以设置进度为文件数量,每次释放一个文件 +1进度 tmp=100
 			ctd.createProgress( 40, 120, "extract",100 );
@@ -269,7 +341,7 @@ namespace ctWin32Wizard
 			t1.detach();
 
 			//创建一个隐藏按钮,可以通过他到page5
-			ctd.createbutton( "继续", 300, 300, PARTCALLBACK( page5 ),85,22,"overbutton" );
+			ctd.createbutton( GETSETUPSTRING(5), 300, 300, PARTCALLBACK( page5 ),85,22,"overbutton" );
 			ShowWindow( ctd.getWnd( "overbutton" ), 0 );
 			return 0;
 		}
@@ -279,14 +351,14 @@ namespace ctWin32Wizard
 			ctd.clearDlg();
 
 			ctd.drawLine( 0, 290, 510, 290, (COLORREF)0xA0A0A0 );
-			ctd.drawBmp( "e:\\WizardImage.bmp", 0, 0, 160, 290 );
+			ctd.drawBmp( TMP_WizardImage, 0, 0, 160, 290 );
 			ctd.setForecolor( RGB( 255, 255, 255 ), {0,0,ctd.hMainDlgRect.right,290} );
-			ctd.createText( "%s 安装向导完成", 180, 20, 300, 22, 16 );
-			ctd.createText( "安装程序已在您的电脑中安装了 %s . 此应用程序可以通过选择安装的快捷方式运行.", 180, 50, 300, 30 );
-			ctd.createText( "单击'完成'退出安装程序.", 180, 85, 300, 30 );
-			ctd.createCheckbox( "运行 %s", 180, 111 );
+			ctd.createText( GETSETUPSTRING(23), 180, 20, 300, 22, 16 );
+			ctd.createText( GETSETUPSTRING(24), 180, 50, 300, 30 );
+			ctd.createText( GETSETUPSTRING(25), 180, 85, 300, 30 );
+			ctd.createCheckbox( GETSETUPSTRING(26), 180, 111 );
 
-			ctd.createbutton( "完成(F)", 300, 300,PARTCALLBACK( end ) );
+			ctd.createbutton( GETSETUPSTRING(7), 300, 300,PARTCALLBACK( end ) );
 			return 0;
 		}
 
@@ -316,11 +388,11 @@ namespace ctWin32Wizard
 		}
 		int CALLBACK cancel( HWND hDlg, DWORD windowId )
 		{
-			std::string msg = "安装未完成!如果你现在退出,程序将无法完成!";
-			msg += "\n\n";
-			msg += "要退出安装吗?";
+			std::string msg = GETSETUPSTRING(28);
+			msg += "\n";
+			msg += GETSETUPSTRING(29);
 
-			if(MessageBoxA( hDlg, msg.c_str(),"退出安装", MB_ICONQUESTION | MB_YESNO ) == IDYES)
+			if(MessageBoxA( hDlg, msg.c_str(), GETSETUPSTRING(27), MB_ICONQUESTION | MB_YESNO ) == IDYES)
 			{
 				PostQuitMessage( 0 );
 			}
